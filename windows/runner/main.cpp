9c362c2e4e38fe3b3d0b3d347b6ee030c00cdfad
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2026-04-07 14:23:42 +0800

    Refactor code structure for improved readability and maintainability

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"ncaf_timer", origin, size)) {
+  if (!window.Create(L"NCAF 2026 Timer", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
